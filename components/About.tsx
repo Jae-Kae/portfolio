b0a7dde7521adb043e68059fb0169e811a850e7a
Janette King <janette@haply.co>
2023-11-16 18:59:35 -0800

    motion added to about section

--- components/About.tsx
+++ components/About.tsx
@@ -6,7 +6,12 @@ type Props = {}
 
 const About = (props: Props) => {
   return (
-    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'
+    >
       <h3 className='absolute top-24 uppercase tracking-[20px] text-[#cad2c5] text-2xl'>
         About
       </h3>
@@ -43,7 +48,7 @@ const About = (props: Props) => {
           ipsum eveniet.
         </p>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
