6c91d0b01e1d067f050e69c359cd475801f5dd4b
Janette King <janette@haply.co>
2023-11-17 14:21:45 -0800

    initial skills and skill components styled

---
+++ components/Skills.tsx
@@ -0,0 +1,42 @@
+'use client'
+import React from 'react'
+import { motion } from 'framer-motion'
+import Skill from './Skill'
+
+type Props = {}
+
+const Skills = (props: Props) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.2 }}
+      className='h-screen flex flex-col relative text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center '
+    >
+      <h3 className='sectionHeader'>Skills</h3>
+      <h3 className='absolute top-36 uppercase tracking-[3px] text-[#84A98C]/70 text-sm'>
+        Hover over a skill for current profiecency
+      </h3>
+      <div className='grid grid-cols-4 gap-5'>
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+      </div>
+    </motion.div>
+  )
+}
+
+export default Skills
