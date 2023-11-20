fff08cb3afab1336a6c143ad32406ad6e906f6c0
Janette King <janette@haply.co>
2023-11-20 15:23:49 -0800

    footer link

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Projects from '@/components/Projects'
 import Skills from '@/components/Skills'
 import WorkExperience from '@/components/WorkExperience'
 import Image from 'next/image'
+import Link from 'next/link'
 
 // pallet https://coolors.co/palette/cad2c5-84a98c-52796f-354f52-2f3e46
 
@@ -41,6 +42,19 @@ const Home = () => {
       <section id='contact' className='snap-start'>
         <ContactMe />
       </section>
+
+      <Link href='#hero'>
+        <footer className='sticky bottom-5 w-full cursor-pointer'>
+          <div className='flex items-center justify-center gap-2'>
+            Built by Janette King
+            <img
+              src='https://media.licdn.com/dms/image/D5603AQEpkYbzWs7gzw/profile-displayphoto-shrink_800_800/0/1664904622974?e=1705536000&v=beta&t=ooGalYlf2TuufVlpFC09mcEN3v49Z4Y0fTu3ITigc68'
+              alt='Logo'
+              className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
+            />
+          </div>
+        </footer>
+      </Link>
     </div>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { SocialIcon } from 'react-social-icons'
+import Link from 'next/link'
 
 type Props = {}
 
@@ -41,31 +42,33 @@ const Header = (props: Props) => {
         />
       </motion.div>
 
-      <motion.div
-        initial={{
-          x: 500,
-          opacity: 0,
-          scale: 0.5,
-        }}
-        animate={{
-          x: 0,
-          opacity: 1,
-          scale: 1,
-        }}
-        transition={{
-          duration: 1.5,
-        }}
-      >
-        <SocialIcon
-          className='cursor-pointer'
-          network='email'
-          fgColor='gray'
-          bgColor='transparent'
-        />
-        <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
-          GET IN TOUCH
-        </p>
-      </motion.div>
+      <Link href='#contact'>
+        <motion.div
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{
+            duration: 1.5,
+          }}
+        >
+          <SocialIcon
+            className='cursor-pointer'
+            network='email'
+            fgColor='gray'
+            bgColor='transparent'
+          />
+          <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
+            GET IN TOUCH
+          </p>
+        </motion.div>
+      </Link>
     </header>
   )
 }
