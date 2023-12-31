971fa64d2a501856b92c6114a5cfe681706b1e55
Janette King <janette@haply.co>
2023-12-30 21:07:46 -0800

    delete sanity

--- sanity/schema.ts
+++
@@ -1,9 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-
-import pageInfo from './schemas/pageInfo'
-import experience from './schemas/experience'
-import project from './schemas/project'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [pageInfo, experience, project],
-}
